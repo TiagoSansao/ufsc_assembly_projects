50332fd4057242fb21889f1a7b81652b0d3aca2e
Tiago Schulz Sansao <tiago.sansao@ufsc.br>
2025-05-29 20:00:03 -0300

    wip(prime_number.asm)

--- prime_number/prime_number.asm
+++ prime_number/prime_number.asm
@@ -1,6 +1,6 @@
 .data
-	not_a_prime_number_msg: .word "Não é um número primo."
-	it_is_a_prime_number_msg: .word "É um número primo."
+	not_a_prime_number_msg: .asciiz "Não é um número primo."
+	it_is_a_prime_number_msg: .asciiz "É um número primo."
 
 
 .text 
@@ -11,22 +11,28 @@ syscall
 
 move $s0, $v0     # s0 tem o numero p/ ver se é primo
 li $s1, 1         # s1 é o count
-div $t1, $s0, 2
+li $t2, 2
+div $s0, $t2
 mfhi $t1          # remainder tá na t1
-beqz $t1, .NOT_A_PRIME_NUMBER 
+beqz $t1, NOT_A_PRIME_NUMBER 
 
 .WHILE 
-	b
+	bge $s0, $s1, IT_IS_A_PRIME_NUMBER
+	
+	
+	j WHILE
 
 
 
 .NOT_A_PRIME_NUMBER:
 	la $a0, 0(not_a_prime_number_msg)
 	li $v0, 4
+	syscall
 
 .IT_IS_A_PRIME_NUMBER
 	la $a0, 0(it_is_a_prime_number_msg)
 	li $v0, 4
+	syscall
 
 
 
